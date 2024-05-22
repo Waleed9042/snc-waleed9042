575d6162d5d5383fdaa8eac30188c50b7900352b
Waleed  Butt <waleedbutt@Waleeds-MacBook-Air.local>
2024-05-22 23:17:10 +0500

    feature/ssg-test-task: Completed task 7,8

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,43 @@
-import { PrismaClient } from "@prisma/client";
-import { mockUsers } from "@/utils/server/mock-users";
-import { IPerson } from "@/types/person";
-import { Person } from "@/utils/common/person";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const usersData: Prisma.UserCreateInput[] = [
+  {
+    backgroundImageUrl:
+      "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
+    profilePictureUrl:
+      "https://images.squarespace-cdn.com/content/v1/51efe10de4b025019c37bb06/1566986917034-2MGUTZB04MU2GFSPAC10/London-corporate-headshot-photography.jpg",
+    name: "PersonA",
+    title: "Backend Developer",
+    followers: 15000,
+    following: 500,
+  },
+  {
+    backgroundImageUrl:
+      "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
+    profilePictureUrl:
+      "https://images.squarespace-cdn.com/content/v1/51efe10de4b025019c37bb06/1566986917034-2MGUTZB04MU2GFSPAC10/London-corporate-headshot-photography.jpg",
+    name: "PersonB",
+    title: "Full Stack Developer",
+    followers: 20000,
+    following: 1000,
+  },
+];
+
 async function main() {
-  // TODO: Add mock users
-  // await prisma.user.create({
-  // data: mockUsers[Person.PersonA] as unknown as IPerson,
-  // });
-  // await prisma.user.create({
-  //   data: mockUsers[Person.PersonB] as unknown as IPerson,
-  // });
+  console.log(`Start seeding ...`);
+  for (const u of usersData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
+  console.log(`Seeding finished.`);
 }
 
 main()
+  .then((res) => console.log(res))
   .catch((e) => {
     throw e;
   })
--- src/utils/server/db.ts
+++ src/utils/server/db.ts
@@ -1,6 +1,22 @@
-import { Person } from "@/utils/common/person";
-import { mockUsers } from "@/utils/server/mock-users";
+import { PrismaClient } from "@prisma/client";
 
-export const getPersonFromDB = async (person: Person) => {
-  return mockUsers[person];
-};
+const prisma = new PrismaClient();
+
+export async function getPersonFromDB(name: string) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        name: name,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error(
+      "Error occurred while fetching user from the database:",
+      error,
+    );
+    throw new Error("Failed to fetch user from the database.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
